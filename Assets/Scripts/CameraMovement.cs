b6e316f41c1ac42a7022897cc9b896988c418283
chuEmma <52865362+chuEmma@users.noreply.github.com>
2020-05-27 20:40:00 -0700

    Camera boundaries and scene transition added

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,11 +7,13 @@ public class CameraMovement : MonoBehaviour
     public GameObject followTarget; 
     private Vector3 targetPos;
     public float moveSpeed; 
+    public Vector2 maxPosition;
+    public Vector2 minPosition; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // LateUpdate is always comes last
@@ -19,27 +21,9 @@ public class CameraMovement : MonoBehaviour
     {
 
         targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        targetPos.x = Mathf.Clamp(targetPos.x, minPosition.x, maxPosition.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, minPosition.y, maxPosition.y);
         transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
-        // if (transform.position != target.position) {
-        //     Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-        //     transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
-        // }
     }
 
-    // public GameObject followTarget; 
-    // private Vector3 targetPos;
-    // public float moveSpeed; 
-
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
-    //     transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
-    // }
 }
